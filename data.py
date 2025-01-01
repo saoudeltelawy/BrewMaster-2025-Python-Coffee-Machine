177358e2ac8e43d36e8df88a719cb7b86bfea712
Saoud ElTelawy <30501682+saoudeltelawy@users.noreply.github.com>
2025-01-01 18:11:21 +0200

    Add project files
    • Uploaded initial project files, including Python scripts and required data files.
    • Files include the main program logic, resource data, and logo assets.
    • Ensures all dependencies and resources are ready for project initialization.

---
+++ data.py
@@ -0,0 +1,36 @@
+# Machine Recourses [Initial]:
+brewMaster_resources = {
+    "water": 1000,
+    "milk": 1500,
+    "coffee_beans": 500,
+    # "disposable_cups": 10,
+    "money": 0,
+}
+
+# Vs.
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 40,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 150,
+            "milk": 250,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    },
+}
